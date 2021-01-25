9844733f4ea4e6e0d9f16f2d7c98ac960d1248c5
Miguel Santiago <miguelangelsh26@gmail.com>
2021-01-25 16:48:33 -0600

    se incluye la modificacion de stock en productos

--- src/main/java/com/maiksantiago/facturas/backend/controller/FacturaController.java
+++ src/main/java/com/maiksantiago/facturas/backend/controller/FacturaController.java
@@ -1,9 +1,12 @@
 package com.maiksantiago.facturas.backend.controller;
 
 import com.maiksantiago.facturas.backend.model.entity.Factura;
+import com.maiksantiago.facturas.backend.model.entity.Item;
+import com.maiksantiago.facturas.backend.model.entity.Producto;
 import com.maiksantiago.facturas.backend.service.FacturaService;
 import com.maiksantiago.facturas.backend.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -20,25 +23,21 @@ public class FacturaController extends CommonController<Factura, FacturaService>
 
     @PostMapping("/")
     public ResponseEntity<?> crear(@Valid @RequestBody Factura factura, BindingResult result) {
+        Factura nuevaFactura = null;
         if (result.hasErrors()) {
             return this.validar(result);
         }
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(factura));
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> actualizar(@Valid @RequestBody Factura factura, BindingResult result,
-                                        @PathVariable Long id) {
-        if (result.hasErrors()) {
-            return this.validar(result);
+        try {
+            nuevaFactura = this.service.save(factura);
+            for (Item item : factura.getItems()) {
+                Producto producto = this.productoService.findById(item.getProducto().getId());
+                producto.setStock(item.getProducto().getStock() - item.getCantidad());
+                this.productoService.save(producto);
+            }
+        } catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
-        Factura facturaActual = this.service.findById(id);
-        if (facturaActual == null) {
-            return ResponseEntity.noContent().build();
-        }
-        facturaActual.setFechaEmision(factura.getFechaEmision());
-        facturaActual.setCliente(factura.getCliente());
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(facturaActual));
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaFactura);
     }
 
 }
