5114b5b32aef2d31a875f93743f6ebbbe38ee9cf
Miguel Santiago <miguelangelsh26@gmail.com>
2021-01-29 14:03:04 -0600

    se formatea el codigo

--- src/main/java/com/maiksantiago/facturas/backend/controller/FacturaController.java
+++ src/main/java/com/maiksantiago/facturas/backend/controller/FacturaController.java
@@ -10,7 +10,10 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
