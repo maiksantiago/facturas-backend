d9ff4456877fe670fb36d46f95b390c4b480aa5b
Miguel Santiago <miguelangelsh26@gmail.com>
2021-01-30 16:52:49 -0600

    se modifican los datos de las tablas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,17 +2,17 @@
 insert into categorias (nombre, descripcion) values ('Videojuegos', 'Consolas, juegos y accesorios');
 
 -- productos
-insert into productos (nombre, precio, stock, id_categoria) values ('Xbox Series X', 13500, 10, 1);
-insert into productos (nombre, precio, stock, id_categoria) values ('Play Station 5', 15900, 10, 1);
+insert into productos (nombre, precio, stock, id_categoria) values ('Xbox Series S 512 GB', 8499, 8, 1);
+insert into productos (nombre, precio, stock, id_categoria) values ('Xbox Series X 1 TB', 13999, 8, 1);
 
 -- clientes
-insert into clientes (nombre, apellido, direccion, telefono, correo_electronico) values ('Miguel', 'Santiago', 'Cupertino, CA 95014', '1234567890', 'miguel@domain.com');
-insert into clientes (nombre, apellido, direccion, telefono, correo_electronico) values ('Maik', 'Santiago', 'Palo Alto, CA 95020', '0123456789', 'maik@domain.com');
+insert into clientes (nombre, apellido, direccion, telefono, correo_electronico) values ('John', 'Doe', 'Cupertino, CA 95014', '1234567890', 'johndoe@gmail.com');
+insert into clientes (nombre, apellido, direccion, telefono, correo_electronico) values ('Jane', 'Doe', 'Seattle, WA 98109', '0123456789', 'janedoe@gmail.com');
 
 -- facturas
 insert into facturas (fecha_emision, id_cliente) values ('2021-01-18', 1);
 insert into facturas (fecha_emision, id_cliente) values ('2021-01-20', 1);
 
 -- items
-insert into items (cantidad, id_producto, id_factura) values (1, 1, 1);
-insert into items (cantidad, id_producto, id_factura) values (1, 2, 2);
+insert into items (cantidad, id_producto, id_factura) values (2, 1, 1);
+insert into items (cantidad, id_producto, id_factura) values (2, 2, 2);
