9dd935f34c7ff93fc744b6539be5a2ad016f5660
Miguel Santiago <miguelangelsh26@gmail.com>
2021-02-21 17:17:40 -0600

    se modifica la consulta

--- src/main/java/com/maiksantiago/facturas/backend/model/repository/ProductoRepository.java
+++ src/main/java/com/maiksantiago/facturas/backend/model/repository/ProductoRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface ProductoRepository extends CrudRepository<Producto, Long> {
 
-    @Query("select p from Producto p where p.nombre like %?1%")
+    @Query("select p from Producto p where p.nombre like %?1% and p.stock != 0")
     List<Producto> findByNombre(String nombre);
 
 }
